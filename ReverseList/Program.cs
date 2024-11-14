27bc41528a43ac5a31e1ebc25dd3241fc577fba8
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-11-14 12:34:16 +0300

    Add project files.

---
+++ ReverseList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int[] dizi = { 1, 2, 3, 4, 5 };
+
+        int[] tersDizi = new int[dizi.Length];
+
+      
+        for (int i = 0; i < dizi.Length; i++)
+        {
+            tersDizi[i] = dizi[dizi.Length - 1 - i];
+        }
+
+        // Ters çevrilmiş diziyi konsola yazdır
+        foreach (var item in tersDizi)
+        {
+            Console.Write(item + " ");
+
+        }
+        Console.ReadKey();
+    }
+}
